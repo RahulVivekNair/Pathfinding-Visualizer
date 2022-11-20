969ab5bfafa55184b72d96f7955a286b37b6c804
RahulVivekNair <68507071+RahulVivekNair@users.noreply.github.com>
2022-11-20 13:20:03 +0530

    includes djikstras algorithm

--- game.py
+++ game.py
@@ -13,6 +13,8 @@ box_width= window_width // columns
 box_height= window_height // rows
 
 grid=[]
+queue=[]
+path=[]
 
 class Box:
     def __init__(self,i,j):
@@ -21,8 +23,23 @@ class Box:
         self.wall=False
         self.starting=False
         self.ending=False
+        self.queued=False
+        self.visited=False
+        self.prior=None
+        self.neighbours=[]
+
     def draw(self,win,color):
         pygame.draw.rect(win,color,(self.x*box_width,self.y*box_height,box_width-2,box_height-2))
+
+    def set_neighbours(self):
+        if self.x > 0:
+            self.neighbours.append(grid[self.x - 1][self.y])
+        if self.x < columns - 1:
+            self.neighbours.append(grid[self.x + 1][self.y])
+        if self.y > 0:
+            self.neighbours.append(grid[self.x][self.y - 1])
+        if self.y < rows - 1:
+            self.neighbours.append(grid[self.x][self.y + 1])
 
 
 for i in range(rows):
@@ -31,6 +48,9 @@ for i in range(rows):
         arr.append(Box(i, j))
     grid.append(arr)
 
+for i in range(rows):
+    for j in range(columns):
+        grid[i][j].set_neighbours()
 
 
 
@@ -38,7 +58,7 @@ def main():
     begin_search = False
     target_box_set = False
     start_box_set = False
-    #searching = True
+    searching = True
     target_box = None
     start_box = None
     
@@ -56,6 +76,7 @@ def main():
                     start_box.starting = True
                     start_box_set = True
                     start_box.visited = True
+                    queue.append(start_box)
                 if mousearray[2] and not target_box_set:
                     i = x // box_width
                     j = y // box_height
@@ -75,11 +96,38 @@ def main():
             if event.type == pygame.KEYDOWN and target_box_set and start_box_set:
                 begin_search = True
 
+        if begin_search:
+            if len(queue) > 0 and searching:
+                current_box = queue.pop(0)
+                current_box.visited = True
+                if current_box == target_box:
+                    searching = False
+                    while current_box.prior != start_box:
+                        path.append(current_box.prior)
+                        current_box = current_box.prior
+                else:
+                    for neighbour in current_box.neighbours:
+                        if not neighbour.queued and not neighbour.wall:
+                            neighbour.queued = True
+                            neighbour.prior = current_box
+                            queue.append(neighbour)
+            else:
+                if searching:
+                    Tk().wm_withdraw()
+                    messagebox.showinfo("No Solution", "There is no solution!")
+                    searching = False
+
         window.fill((0,0,0))
         for i in range(rows):
             for j in range(columns):
                 box = grid[i][j]
                 box.draw(window, (50, 50, 50))
+                if box.queued:
+                    box.draw(window, (200, 0, 0))
+                if box.visited:
+                    box.draw(window, (0, 200, 0))
+                if box in path:
+                    box.draw(window, (0, 0, 200))
                 if box.starting:
                     box.draw(window, (0, 200, 200))
                 if box.wall:
